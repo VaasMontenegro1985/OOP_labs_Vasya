c70bed2f2a168fc9e819389f0fa3007de4e68d1a
Вася <vasyamontenegro1985@gmail.com>
2024-10-29 16:21:08 +0000

    fix ci again

--- lab1/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/OOP_labs_Vasya/lab1/_deps/googletest-src/googletest
-# Build directory: /workspaces/OOP_labs_Vasya/lab1/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
